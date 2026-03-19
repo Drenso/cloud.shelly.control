28bf8a51e3258ab826675ad76c7daa33debe2403
Bob van de Vijver <bob@drenso.nl>
2026-03-19 11:40:26 +0100

    Switch to shared eslint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,37 +1,3 @@
-import js from '@eslint/js';
-import tseslint from 'typescript-eslint';
+import drensoConfig from '@drenso/eslint-config-homey-mts';
 
-export default tseslint.config(
-  {
-    ignores: ['.homeybuild'],
-  },
-  js.configs.recommended,
-  ...tseslint.configs.recommended,
-  {
-    files: ['**/*.mts'],
-    ignores: ['node_modules', '.homeybuild'],
-    rules: {
-      '@typescript-eslint/explicit-function-return-type': 'error',
-      // Mark as warning to not block during dev work, and allow with description
-      '@typescript-eslint/ban-ts-comment': [
-        'warn',
-        {
-          'ts-ignore': 'allow-with-description',
-        },
-      ],
-      '@typescript-eslint/no-unused-vars': [
-        'warn',
-        {
-          argsIgnorePattern: '^_',
-        },
-      ],
-      '@typescript-eslint/consistent-type-imports': [
-        'error',
-        { prefer: 'type-imports', fixStyle: 'separate-type-imports' },
-      ],
-      '@typescript-eslint/no-import-type-side-effects': 'error',
-      semi: [2, 'always'],
-      'no-extra-semi': 'error',
-    },
-  },
-);
+export default drensoConfig;
