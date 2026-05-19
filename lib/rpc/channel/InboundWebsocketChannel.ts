aba4cdf052807b084607f9fd76043fa35a90ba5d
Joost Loohuis <joost@drenso.nl>
2026-05-19 16:08:08 +0200

    chore: clean up old TODOs

--- lib/rpc/channel/InboundWebsocketChannel.ts
+++ lib/rpc/channel/InboundWebsocketChannel.ts
@@ -29,9 +29,6 @@ type InboundWsChannelMittEvents = {
   opened: undefined;
 };
 
-// TODO authentication
-// See documentation: https://shelly-api-docs.shelly.cloud/gen2/General/Authentication/#authentication
-// See example: https://github.com/home-assistant-libs/aioshelly/blob/main/aioshelly/rpc_device/wsrpc.py
 // TODO wss://
 export default class InboundWebsocketChannel implements RpcChannel {
   public ws!: WebSocket;
