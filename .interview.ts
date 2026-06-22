c7aa600a39167a77cd06079f2bf742f84d8055d1
Joost Loohuis <joost@drenso.nl>
2026-06-22 10:00:00 +0200

    fix: add null password to interview script

--- .interview.ts
+++ .interview.ts
@@ -15,7 +15,7 @@ const address = process.argv[2];
 const mockTranslate = (key: string): string => key;
 
 // TODO add password
-const rpcChannel = new HttpChannel(address, console.log, mockTranslate, false);
+const rpcChannel = new HttpChannel(address, console.log, mockTranslate, false, null);
 
 const deviceInfo = await Shelly.GetDeviceInfo(rpcChannel);
 const deviceType = deviceInfo.result.id.split('-')[0];
