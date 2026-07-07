79a77e29fa5cfc4a23390290b47106f188a008d3
Joost Loohuis <joost@drenso.nl>
2026-07-07 11:19:37 +0200

    feat(bluetooth): remove event listener when device is removed

--- lib/ble/BleDevice.ts
+++ lib/ble/BleDevice.ts
@@ -7,6 +7,11 @@ import type ShellyBleDriver from './BleDriver.js';
 export default abstract class ShellyBleDevice extends Homey.Device {
   declare public readonly __id: string;
 
+  public constructor(...args: unknown[]) {
+    super(...(args as never[]));
+    this.handleBleForward = this.handleBleForward.bind(this);
+  }
+
   public get app(): ShellyApp {
     return this.homey.app as ShellyApp;
   }
@@ -17,7 +22,12 @@ export default abstract class ShellyBleDevice extends Homey.Device {
 
   public async onInit(): Promise<void> {
     const macAddress = this.getTypedData().id.toLowerCase();
-    this.app.btHomeServer.btHomeMitt.on(macAddress, this.handleBleForward.bind(this));
+    this.app.btHomeServer.btHomeMitt.on(macAddress, this.handleBleForward);
+  }
+
+  public async onDeleted(): Promise<void> {
+    const macAddress = this.getTypedData().id.toLowerCase();
+    this.app.btHomeServer.btHomeMitt.off(macAddress, this.handleBleForward);
   }
 
   protected async handleBleForward(data: BleForwardEventData): Promise<void> {
