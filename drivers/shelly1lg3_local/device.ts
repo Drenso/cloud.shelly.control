af7d5bdd6672165eedf630a413231fb96ff5b08a
Joost Loohuis <joost@drenso.nl>
2026-07-31 15:29:04 +0200

    feat: add 1L gen3

---
+++ drivers/shelly1lg3_local/device.ts
@@ -0,0 +1,3 @@
+import ShellyLocalDevice from '../../lib/local/LocalDevice.js';
+
+export default class Shelly1LGen3LocalDevice extends ShellyLocalDevice {}
---
+++ drivers/shelly1lg3_local/driver.ts
@@ -0,0 +1,3 @@
+import ShellyLocalDriver from '../../lib/local/LocalDriver.js';
+
+export default class Shelly1LGen3LocalDriver extends ShellyLocalDriver {}
