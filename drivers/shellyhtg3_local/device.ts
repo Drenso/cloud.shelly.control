c292c7aa0e114c91fd27998631dde99c744d5548
Bob van de Vijver <bob@drenso.nl>
2026-08-07 12:23:19 +0200

    device: Add H&T Gen3 LAN support

---
+++ drivers/shellyhtg3_local/device.ts
@@ -0,0 +1,3 @@
+import ShellyLocalDevice from '../../lib/local/LocalDevice.js';
+
+export default class HTGen3LocalDevice extends ShellyLocalDevice {}
---
+++ drivers/shellyhtg3_local/driver.ts
@@ -0,0 +1,5 @@
+import ShellyLocalDriver from '../../lib/local/LocalDriver.js';
+
+export default class HTGen3LocalDriver extends ShellyLocalDriver {
+  protected batteryDevice = true;
+}
