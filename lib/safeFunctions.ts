a6889848684cd5efbee44adc39f38c14abc461fb
Bob van de Vijver <bob@drenso.nl>
2026-08-14 18:40:13 +0200

    chore: Log triggered flows when in debug mode

--- lib/safeFunctions.ts
+++ lib/safeFunctions.ts
@@ -1,4 +1,4 @@
-import type Homey from 'homey';
+import Homey from 'homey';
 
 export async function safeAddCapability(device: Homey.Device, capabilityId: string): Promise<void> {
   if (device.hasCapability(capabilityId)) {
@@ -34,5 +34,9 @@ export async function safeTriggerDeviceCard(
   tokens?: Record<string, unknown>,
   triggerArgs?: Record<string, unknown>,
 ): Promise<void> {
+  if (Homey.env.DEBUG === '1') {
+    device.log('[flow dbg]', flowId, JSON.stringify(tokens), JSON.stringify(triggerArgs));
+  }
+
   return device.homey.flow.getDeviceTriggerCard(flowId).trigger(device, tokens, triggerArgs).catch(device.error);
 }
