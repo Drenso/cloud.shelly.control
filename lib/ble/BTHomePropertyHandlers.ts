407721ce7d9aafe6eb2e8d7943b1391bfcf9050c
Bob van de Vijver <bob@drenso.nl>
2026-08-25 12:12:12 +0200

    feat: Support motion, illuminance and lightLevel BTHome properties

--- lib/ble/BTHomePropertyHandlers.ts
+++ lib/ble/BTHomePropertyHandlers.ts
@@ -1,7 +1,8 @@
 import { type ButtonEventType, safeTriggerButtonPressed, safeTriggerSingleButtonPressed } from '../flow/buttonFlows.js';
+import type { ShellyIlluminanceType } from '../flow/illuminanceFlows.js';
 import { safeSetCapabilityValue } from '../safeFunctions.js';
 import type ShellyBleDevice from './BleDevice.js';
-import { BTHomeButtonEventType, type BTHomeData } from './BTHome.js';
+import { BTHomeButtonEventType, type BTHomeData, BTHomeLightLevel } from './BTHome.js';
 
 export async function handleBatteryProperty(device: ShellyBleDevice, btHomeData: BTHomeData): Promise<void> {
   if (btHomeData.battery?.length !== 1) {
@@ -11,12 +12,36 @@ export async function handleBatteryProperty(device: ShellyBleDevice, btHomeData:
   await safeSetCapabilityValue(device, 'measure_battery', btHomeData.battery[0]);
 }
 
+export async function handleIlluminanceProperty(device: ShellyBleDevice, btHomeData: BTHomeData): Promise<void> {
+  if (btHomeData.illuminance?.length !== 1) {
+    return;
+  }
+
+  await safeSetCapabilityValue(device, 'measure_luminance', btHomeData.illuminance[0]);
+}
+
+export async function handleLightLevelProperty(device: ShellyBleDevice, btHomeData: BTHomeData): Promise<void> {
+  if (btHomeData.lightLevel?.length !== 1) {
+    return;
+  }
+
+  await safeSetCapabilityValue(device, 'shelly_illumination', convertLightLevel(btHomeData.lightLevel[0]));
+}
+
+export async function handleMotionProperty(device: ShellyBleDevice, btHomeData: BTHomeData): Promise<void> {
+  if (btHomeData.motion?.length !== 1) {
+    return;
+  }
+
+  await safeSetCapabilityValue(device, 'alarm_motion', btHomeData.motion[0]);
+}
+
 export async function handleSingleButtonEventProperty(device: ShellyBleDevice, btHomeData: BTHomeData): Promise<void> {
   if (btHomeData.buttonEvent?.length !== 1) {
     return;
   }
 
-  const eventType = await convertButtonEventType(btHomeData.buttonEvent[0]);
+  const eventType = convertButtonEventType(btHomeData.buttonEvent[0]);
   if (!eventType) {
     return;
   }
@@ -32,7 +57,7 @@ export async function handleButtonEventProperty(device: ShellyBleDevice, btHomeD
 
   for (let buttonIndex = 0; buttonIndex < buttonCount; buttonIndex++) {
     const buttonEvent: BTHomeButtonEventType = btHomeData.buttonEvent![buttonIndex];
-    const eventType = await convertButtonEventType(buttonEvent);
+    const eventType = convertButtonEventType(buttonEvent);
     if (!eventType) {
       continue;
     }
@@ -41,7 +66,7 @@ export async function handleButtonEventProperty(device: ShellyBleDevice, btHomeD
   }
 }
 
-async function convertButtonEventType(buttonEvent: BTHomeButtonEventType): Promise<ButtonEventType | null> {
+function convertButtonEventType(buttonEvent: BTHomeButtonEventType): ButtonEventType | null {
   switch (buttonEvent) {
     case BTHomeButtonEventType.None:
       return null;
@@ -60,5 +85,16 @@ async function convertButtonEventType(buttonEvent: BTHomeButtonEventType): Promi
     case BTHomeButtonEventType.HoldPress:
     case BTHomeButtonEventType.HoldPress2:
       return 'hold';
+  }
+}
+
+function convertLightLevel(lightLevelEvent: BTHomeLightLevel): ShellyIlluminanceType {
+  switch (lightLevelEvent) {
+    case BTHomeLightLevel.Dark:
+      return 'dark';
+    case BTHomeLightLevel.Twilight:
+      return 'twilight';
+    case BTHomeLightLevel.Bright:
+      return 'bright';
   }
 }
