7778e5ae241807b2756278b7eb2d2b550181cb12
Bob van de Vijver <bob@drenso.nl>
2026-08-26 17:02:46 +0200

    device: Add BLE support for BLU Button 1

--- drivers/shellyblubutton1_ble/driver.ts
+++ drivers/shellyblubutton1_ble/driver.ts
@@ -1,6 +1,6 @@
 import ShellyBleDriver from '../../lib/ble/BleDriver.js';
 
 export default class ShellyBluButtonThough1BleDriver extends ShellyBleDriver {
-  // Same as BLU Button 1, but separate drivers for the different images and icon
+  // Same as BLU Button Tough 1, but separate drivers for the different images and icon
   public bleNamePrefixes = ['SBBT-002C'];
 }
