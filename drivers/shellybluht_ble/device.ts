422e4cd116e55162bbe86930cfe4770028259153
Bob van de Vijver <bob@drenso.nl>
2026-08-26 17:25:27 +0200

    device: Add BLE support for BLU H&T

---
+++ drivers/shellybluht_ble/device.ts
@@ -0,0 +1,23 @@
+import ShellyBleDevice from '../../lib/ble/BleDevice.js';
+import type { BTHomeData } from '../../lib/ble/BTHome.js';
+import {
+  handleBatteryProperty,
+  handleHumidityProperty,
+  handleSingleButtonEventProperty,
+  handleTemperatureProperty,
+} from '../../lib/ble/BTHomePropertyHandlers.js';
+import type { ButtonEventTypesDeviceInterface } from '../../lib/capabilityInterfaces.js';
+import type { ButtonEventType } from '../../lib/flow/buttonFlows.js';
+
+export default class ShellyBluHTBleDevice extends ShellyBleDevice implements ButtonEventTypesDeviceInterface {
+  public async handleBtHomeForward(btHomeData: BTHomeData): Promise<void> {
+    await handleBatteryProperty(this, btHomeData);
+    await handleHumidityProperty(this, btHomeData);
+    await handleTemperatureProperty(this, btHomeData);
+    await handleSingleButtonEventProperty(this, btHomeData);
+  }
+
+  public getButtonEventTypes(): ButtonEventType[] {
+    return ['single_press', 'hold'];
+  }
+}
