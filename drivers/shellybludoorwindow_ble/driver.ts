28a8503cf2e1f78b3f0ab96d07e4592c842e7e04
Bob van de Vijver <bob@drenso.nl>
2026-08-27 14:27:50 +0200

    device: Add BLE support for BLU Door/Window

---
+++ drivers/shellybludoorwindow_ble/driver.ts
@@ -0,0 +1,5 @@
+import ShellyBleDriver from '../../lib/ble/BleDriver.js';
+
+export default class ShellyBluDoowWindowBleDriver extends ShellyBleDriver {
+  public bleNamePrefixes = ['SBDW-002C'];
+}
