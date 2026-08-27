02007809b6680e61d4dc3f508affe96f9fb5d945
Bob van de Vijver <bob@drenso.nl>
2026-08-27 11:41:06 +0200

    device: Add BLE support for BLU H&T Display ZB

---
+++ drivers/shellybluhtdisplayzb_ble/driver.ts
@@ -0,0 +1,5 @@
+import ShellyBleDriver from '../../lib/ble/BleDriver.js';
+
+export default class ShellyBluHTDisplayZBBleDriver extends ShellyBleDriver {
+  public bleNamePrefixes = ['SBHT-103C'];
+}
