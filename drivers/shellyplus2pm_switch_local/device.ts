a61e2f6833222304716a1dd6fffc40523a1f66a6
Joost Loohuis <joost@drenso.nl>
2026-08-31 15:56:39 +0200

    device(plus 2pm): add plus 2pm LAN switch mode support

---
+++ drivers/shellyplus2pm_switch_local/device.ts
@@ -0,0 +1,3 @@
+import ShellyLocalDevice from '../../lib/local/LocalDevice.js';
+
+export default class ShellyPlus2PMSwitchLocalDevice extends ShellyLocalDevice {}
---
+++ drivers/shellyplus2pm_switch_local/driver.ts
@@ -0,0 +1,64 @@
+import ShellyLocalDriver from '../../lib/local/LocalDriver.js';
+import type { ShellyLocalListDeviceProperties } from '../../lib/types.js';
+import type { ShellyGetComponentsResponseComponent } from '../../lib/component/components/Shelly/GetComponents.js';
+import Switch, { type SwitchConfig } from '../../lib/component/components/Switch.js';
+import type { ShellyGetDeviceInfoResponse } from '../../lib/component/components/Shelly/GetDeviceInfo.js';
+
+export default class ShellyPlus2PMSwitchLocalDriver extends ShellyLocalDriver {
+  protected async onPairMatchDevice(deviceInfo: ShellyGetDeviceInfoResponse): Promise<boolean> {
+    return deviceInfo.id.toLowerCase().startsWith(this.baseDriverId) && deviceInfo.profile === 'switch';
+  }
+
+  public async assembleHomeyDevices(
+    selectedDevice: ShellyLocalListDeviceProperties,
+    components: ShellyGetComponentsResponseComponent[],
+  ): Promise<ShellyLocalListDeviceProperties[]> {
+    const componentMapping: Record<string, ShellyGetComponentsResponseComponent> = {};
+
+    for (const component of components) {
+      componentMapping[component.key] = component;
+    }
+
+    const splitComponents = ['switch', 'input'];
+
+    const subDevices: ShellyLocalListDeviceProperties[] = [];
+    const id = selectedDevice.data.id;
+    // Create a sub-device for each switch
+    for (const component of components) {
+      const [componentType, componentId] = component.key.split(':') as [string, `${number}` | undefined];
+      if (componentType === 'switch') {
+        const switchId = parseInt(componentId!, 10);
+        const subdeviceId = `${id}:switch:${switchId}`;
+
+        const switchConfig = component.config as SwitchConfig;
+        const subdeviceName = switchConfig.name ?? `${Switch.uiName} ${switchId + 1}`;
+
+        subDevices.push({
+          name: `${selectedDevice.name} - ${subdeviceName}`,
+          data: {
+            id: subdeviceId,
+            parent: selectedDevice.data.id,
+            subdevice_id: parseInt(componentId!, 10),
+          },
+          icon: `../../../assets/drivers/${this.baseDriverId}/icon.svg`,
+          store: {
+            ...selectedDevice.store,
+            components: splitComponents.map(splitComponent => `${splitComponent}:${componentId}`),
+          },
+          capabilities: ['button.restart'],
+        });
+      }
+    }
+
+    // Assign components that do not belong to a specific switch to all sub-devices
+    for (const component of components) {
+      const [componentType] = component.key.split(':') as [string, `${number}` | undefined];
+      if (!splitComponents.includes(componentType)) {
+        for (const subDevice of subDevices) {
+          subDevice.store.components.push(component.key);
+        }
+      }
+    }
+    return subDevices;
+  }
+}
