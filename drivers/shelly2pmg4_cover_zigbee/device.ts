0e60ac92cf1843f3d78c2b1e499cb3ecfe9c6f72
Joost Loohuis <joost@drenso.nl>
2026-09-15 18:13:26 +0200

    fix: use correct attribute for checking 2pm gen4 mode in Zigbee

--- drivers/shelly2pmg4_cover_zigbee/device.ts
+++ drivers/shelly2pmg4_cover_zigbee/device.ts
@@ -1,13 +1,13 @@
 import initWindowCoveringsDevice from '@drenso/homey-zigbee-library/capabilities/windowCoverings.mjs';
-import zbClusters, { type ZCLNode } from 'zigbee-clusters';
+import zbClusters, { type WindowCoveringCluster, type ZCLNode } from 'zigbee-clusters';
 import ShellyZigbeeDevice from '../../lib/zigbee/ZigbeeDevice.js';
 
 export default class Shelly2PMGen4CoverZigbeeDevice extends ShellyZigbeeDevice {
   protected async configureDevice(zclNode: ZCLNode): Promise<void> {
+    const cluster = zclNode.endpoints[1].clusters[zbClusters.WindowCoveringCluster.NAME] as WindowCoveringCluster;
+
     try {
-      await zclNode.endpoints[1].clusters[zbClusters.WindowCoveringCluster.NAME]?.readAttributes([
-        'currentPositionLift1',
-      ]);
+      await cluster?.readAttributes(['currentPositionLift']);
     } catch (error) {
       if (error instanceof Error && error.message === 'UNSUPPORTED_CLUSTER') {
         this.log('Marking as unavailable, wrong type selected by user');
