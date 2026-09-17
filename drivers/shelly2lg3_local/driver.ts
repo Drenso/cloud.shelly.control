1bc57352509608f582ef90bd756639470736f636
Bob van de Vijver <bob@drenso.nl>
2026-09-17 16:23:10 +0200

    device(2L Gen3): Add LAN support

---
+++ drivers/shelly2lg3_local/driver.ts
@@ -0,0 +1,3 @@
+import ShellyDoubleSwitchInputLocalDriver from '../../lib/local/ShellyDoubleSwitchInputLocalDriver.js';
+
+export default class Shelly2LGen3LocalDriver extends ShellyDoubleSwitchInputLocalDriver {}
